9e63104cfce3968edb1a11868d58b1b5ff8ed949
Nitin Bopche <nikbopche125256@gmail.com>
2026-05-29 14:34:21 +0530

    Updated deployement settings.py

--- djfintracker/settings.py
+++ djfintracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d!gb=t&*4(ed0v2l0rtrd_07h^#=kd86&k)l2a8s408ekcsp)s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nitinb.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
